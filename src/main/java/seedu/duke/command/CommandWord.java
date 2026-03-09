337a0a5a06c3519d2ebd73f95a0aa7f68bed07d4
Wesley <league4orce@gmail.com>
2026-03-09 17:31:43 +0800

    Add description text for each command

--- src/main/java/seedu/duke/command/CommandWord.java
+++ src/main/java/seedu/duke/command/CommandWord.java
@@ -1,12 +1,33 @@
 package seedu.duke.command;
 
 public enum CommandWord {
-    HELP, EXIT,
+    HELP("help", "lists all commands"),
+    EXIT("exit", "exits the program"),
 
     // Wallet related commands
-    CREATE, LIST, KEYGEN,
-    BALANCE, SEND,
+    CREATE("create", "creates a new wallet"),
+    LIST("list", "lists all your wallets"),
+    KEYGEN("keygen", "generates and displays key pair"),
+    BALANCE("balance", "displays balance of wallet"),
+    SEND("send", "sends cryptocurrency from one wallet to another"),
 
     // Blockchain related commands
-    VALIDATE, VIEWBLOCK
+    VALIDATE("validate", "validates block chain integrity"),
+    VIEWBLOCK("viewblock", "displays details of the block");
+
+    private final String command;
+    private final String description;
+
+    CommandWord(String command, String description) {
+        this.command = command;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCommand() {
+        return command;
+    }
 }
