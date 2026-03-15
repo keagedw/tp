84aae0b58511edd7fe328b2d8f399f3234774f75
Wesley <135317942+Trivorten-wes@users.noreply.github.com>
2026-03-16 02:24:19 +0800

    Update Duke.java
    Edit duke file to call the correct parameters

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -23,7 +23,7 @@ public class Duke {
             String message = in.nextLine().strip();
             try {
                 String[] components = message.split("\\s+", 2);
-                Command c = Parser.parse(components[0]);
+                Command c = Parser.parse(components[0], blockchain);
                 if (c instanceof ExitCommand) {
                     break;
                 }
